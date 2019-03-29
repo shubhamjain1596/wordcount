c56e25c26c2024491965c0147da9f317cea3d2e3
shubhamjain1596 <bittu1946@gmail.com>
2019-03-29 18:04:33 +0530

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request,'home.html',)
+def count(request):
+    data=request.GET['fulltextarea']
+    word_list=data.split()
+    list_length=len(word_list)
+    worddictonary={}
+    for word in word_list:
+        if word in worddictonary:
+            worddictonary[word]+=1
+        else:
+            worddictonary[word] = 1
+
+    sorted_list=sorted(worddictonary.items(),key=operator.itemgetter(1),reverse=True)
+    return render(request,'count.html',{'fulltext':data,'words':list_length,'worddictionary':sorted_list})
